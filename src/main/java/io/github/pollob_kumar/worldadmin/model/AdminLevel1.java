a153f7bc721ff72176d82fc6d81b2f1d4765fb12
Pollob-Kumar <pkspollob14@gmail.com>
2026-04-14 00:08:11 +0600

    feat: added initial admin unit model classes

---
+++ src/main/java/io/github/pollob_kumar/worldadmin/model/AdminLevel1.java
@@ -0,0 +1,6 @@
+package io.github.pollob_kumar.worldadmin.model;
+
+// Model, data structure define kore.
+public class AdminLevel1 extends AdminUnit {
+    // No extra field needed for top level
+}
