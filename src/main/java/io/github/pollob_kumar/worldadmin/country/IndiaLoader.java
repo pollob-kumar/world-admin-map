43ff116470fcb1e0eb953611e88d2c0a5e6126f8
Pollob-Kumar <pkspollob14@gmail.com>
2026-04-19 00:40:35 +0600

    feat(country): added India loader placeholder
       - implemented CountryLoader for India with explicit UnsupportedCountryException
       - meet SRS requirement for a stub loader until India data is available

--- src/main/java/io/github/pollob_kumar/worldadmin/country/IndiaLoader.java
+++ src/main/java/io/github/pollob_kumar/worldadmin/country/IndiaLoader.java
@@ -1,4 +1,19 @@
 package io.github.pollob_kumar.worldadmin.country;
 
-public class IndiaLoader {
+import io.github.pollob_kumar.worldadmin.enums.CountryCode;
+import io.github.pollob_kumar.worldadmin.exception.UnsupportedCountryException;
+import io.github.pollob_kumar.worldadmin.repository.AdminRepository;
+
+public class IndiaLoader implements CountryLoader {
+    @Override
+    public CountryCode getCountryCode() {
+        return CountryCode.IN;
+    }
+
+    @Override
+    public AdminRepository load() {
+        throw new UnsupportedCountryException(
+                "India (IN) data is not yet available. Planned for a future release."
+        );
+    }
 }
