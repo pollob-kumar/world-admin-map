46de5382c0a3f41d3547a0b92e4cc0bbbbe87535
Pollob-Kumar <pkspollob14@gmail.com>
2026-04-19 01:05:03 +0600

    feat(factory): implemented country loader factory
       - register BD/IN/US loaders in a static map
       - add loader lookup with unsupported-country guard and supported list

--- src/main/java/io/github/pollob_kumar/worldadmin/factory/CountryFactory.java
+++ src/main/java/io/github/pollob_kumar/worldadmin/factory/CountryFactory.java
@@ -1,4 +1,37 @@
 package io.github.pollob_kumar.worldadmin.factory;
 
+import io.github.pollob_kumar.worldadmin.country.BangladeshLoader;
+import io.github.pollob_kumar.worldadmin.country.CountryLoader;
+import io.github.pollob_kumar.worldadmin.country.IndiaLoader;
+import io.github.pollob_kumar.worldadmin.country.USALoader;
+import io.github.pollob_kumar.worldadmin.enums.CountryCode;
+import io.github.pollob_kumar.worldadmin.exception.UnsupportedCountryException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountryFactory {
+    private static final Map<CountryCode, CountryLoader> LOADERS;
+
+    static {
+        Map<CountryCode, CountryLoader> map = new EnumMap<>(CountryCode.class);
+        map.put(CountryCode.BD, new BangladeshLoader());
+        map.put(CountryCode.IN, new IndiaLoader());
+        map.put(CountryCode.US, new USALoader());
+        LOADERS = Collections.unmodifiableMap(map);
+    }
+
+    public static CountryLoader getLoader(CountryCode code) {
+        CountryLoader loader = LOADERS.get(code);
+        if (loader == null) {
+            throw new UnsupportedCountryException("No loader found for: " + code);
+        }
+        return loader;
+    }
+
+    public static List<CountryCode> getSupportedCountries() {
+        return Collections.unmodifiableList(new ArrayList<>(LOADERS.keySet()));
+    }
 }
